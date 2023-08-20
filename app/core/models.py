53be2ed746b9434f3159533eabf9a6cf0a4a55bb
ganu07 <gborhade7@gmail.com>
2023-08-20 11:39:40 +0530

    added recipe app test cases

--- app/core/models.py
+++ app/core/models.py
@@ -8,6 +8,7 @@ from django.contrib.auth.models import (
     BaseUserManager,
     PermissionsMixin
 )
+from django.conf import settings
 
 
 class Usermanager(BaseUserManager):
@@ -42,3 +43,19 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = Usermanager()
 
     USERNAME_FIELD = 'email'
+
+
+class Recipe(models.Model):
+    """Receipe object"""
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.title
